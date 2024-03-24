0ae9a5c712cb862c124fa1413c68cf026fde3781
Song Min Gyeom <123460277+odd-mune@users.noreply.github.com>
2024-03-25 02:58:35 +0900

    카메라, 플레이어 기본 공격
    - 카메라 (smooth, bound) 추가
    - signs and Dialog (미완)
    - 플레이어 기본 공격 (애니메이션, 키) 추가

---
+++ Assets/1_Script/Sign.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool dialogActive;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))    
+        {
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Player left range");
+        }
+        
+    }
+}
