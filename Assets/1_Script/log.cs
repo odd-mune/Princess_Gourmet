c1e8f0f5cf1d1a37aba58b53801e7bceaeb51f69
Song Min Gyeom <123460277+odd-mune@users.noreply.github.com>
2024-04-03 15:37:42 +0900

    공주방 새로 생김
    -공주방 타일 깔기 (아직 아트 오류 있음)
    -공주 Idle 둠칫 애니메이션 추가 완료
    -Enemy AI (반경에 접근하면 쫒아옴)
    -카메라 지금 빼놓은 상태. (maxmin때문에)

---
+++ Assets/1_Script/log.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log : Enemy
+{
+    public Transform target;
+    public float chaseRadius;
+    public float attackRadius;
+    public Transform homePosition;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius 
+            && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
