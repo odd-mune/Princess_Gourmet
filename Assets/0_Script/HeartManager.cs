b2f7cecd75552e0cb41e9d5c0659ee0eb600f8e0
Song Min Gyeom <123460277+odd-mune@users.noreply.github.com>
2024-04-21 04:54:29 +0900

    Health System 완성
    - 플레이어 데미지 입음. 사망 가능

---
+++ Assets/0_Script/HeartManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartManager : MonoBehaviour
+{
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite halfFullHeart;
+    public Sprite emptyHeart;
+    public FloatValue heartContainers;
+    public FloatValue playerCurrentHealth;
+
+    void Start()
+    {
+        InitHearts();
+    }
+
+    public void InitHearts()
+    {
+        for (int i = 0; i < heartContainers.initialValue; i ++)
+        {
+            hearts[i].gameObject.SetActive(true);
+            hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        float tempHealth = playerCurrentHealth.initialValue / 2;
+        for (int i = 0; i < heartContainers.initialValue; i++)
+        {
+            if(i <= tempHealth)
+            {
+                //Full Heart
+                hearts[i].sprite = fullHeart;
+            }
+            else if(i > tempHealth)
+            {
+                //empty heart
+                hearts[i].sprite = emptyHeart;
+            }
+            else
+            {
+                //half full heart
+                hearts[i].sprite = halfFullHeart;
+            }
+        }
+    }
+}
