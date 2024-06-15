2e1ccea475f71546a54f31c94c6ecf7602e2e913
Song Min Gyeom <123460277+odd-mune@users.noreply.github.com>
2024-06-15 11:54:03 +0900

    .

--- Assets/0_Script/Game Stuff/DialogueManager/TalkManager.cs
+++ Assets/0_Script/Game Stuff/DialogueManager/TalkManager.cs
@@ -23,24 +23,24 @@ public class TalkManager : MonoBehaviour
         talkData.Add(2, new string[] {"요리왕국 인기 Top 100 요리레시피"});
         talkData.Add(3, new string[] {"공주의 비밀 일기장"});
         talkData.Add(4, new string[] {"옷을 갈아입을 수 있는 옷장이다."});
-        talkData.Add(5, new string[] {"마을로 가는 길"});
+        talkData.Add(5, new string[] {"<- 마을로 가는 길", "숲으로 가는 길 ->"});
         talkData.Add(6, new string[] {"숲으로 가는 길"});
 
         // NPC 대화 
             // 왕궁 경비병 1
             talkData.Add(1000, new string[] {"안녕하십니까! 공주님:0", "지금은 알현실에 들어가실 수 없으십니다.:2"});
             // 왕궁 하녀 1
-            talkData.Add(1100, new string[] {"안녕하세요 공주님! 좋은 하루입니다!:4", "오늘따라 일이 많네요:7"});
+            talkData.Add(2000, new string[] {"안녕하세요 공주님! 좋은 하루입니다!:4", "오늘따라 일이 많네요:7"});
 
         // 초상화 
         portraitData.Add(1000 + 0, portraitArr[0]);
         portraitData.Add(1000 + 1, portraitArr[1]);
         portraitData.Add(1000 + 2, portraitArr[2]);
         portraitData.Add(1000 + 3, portraitArr[3]);
-        portraitData.Add(1100 + 0, portraitArr[4]);
-        portraitData.Add(1100 + 1, portraitArr[5]);
-        portraitData.Add(1100 + 2, portraitArr[6]);
-        portraitData.Add(1100 + 3, portraitArr[7]);
+        portraitData.Add(2000 + 0, portraitArr[4]);
+        portraitData.Add(2000 + 1, portraitArr[5]);
+        portraitData.Add(2000 + 2, portraitArr[6]);
+        portraitData.Add(2000 + 3, portraitArr[7]);
     }
 
     public string GetTalk(int id, int talkIndex)
