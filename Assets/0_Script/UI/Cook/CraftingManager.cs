938a5f655131c2499638ea678170ca8e98a82f34
Song Min Gyeom <123460277+odd-mune@users.noreply.github.com>
2024-09-19 21:07:00 +0900

    9/19

--- Assets/0_Script/UI/Cook/CraftingManager.cs
+++ Assets/0_Script/UI/Cook/CraftingManager.cs
@@ -8,6 +8,41 @@ public class CraftingManager : MonoBehaviour
     private Item currentItem;
     public Image customCursor;
 
+    public Slot[] craftingSlots;
+
+    public List<Item> itemList;
+    public string[] recipes;
+    public Item[] recipeResults;
+    public Slot resultSlot;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            if(currentItem != null)
+            {
+                customCursor.gameObject.SetActive(false);
+                Slot nearestSlot = null;
+                float shortestDistance = float.MaxValue;
+
+                foreach(Slot slot in craftingSlots)
+                {
+                    float dist = Vector2.Distance(Input.mousePosition, slot.transform.position);
+
+                    if(dist < shortestDistance)
+                    {
+                        shortestDistance = dist;
+                        nearestSlot = slot;
+                    }
+                }
+                nearestSlot.gameObject.SetActive(true);
+                nearestSlot.GetComponent<Image>().sprite = currentItem.GetComponent<Image>().sprite;
+                nearestSlot.item = currentItem;
+                currentItem = null;
+            }
+        }
+    }
+
     public void OnMouseDownItem(Item item)
     {
         if(currentItem == null)
