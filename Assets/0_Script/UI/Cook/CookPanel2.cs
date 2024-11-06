15ae9744cc0d5e7facae2d5256be253c0942f520
Alegruz <alegruz@khu.ac.kr>
2024-11-06 20:32:09 +0900

    - 시간 지나면 자동으로 요리 화면 끝나도록 수정

--- Assets/0_Script/UI/Cook/CookPanel2.cs
+++ Assets/0_Script/UI/Cook/CookPanel2.cs
@@ -1,19 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CookPanel2 : MonoBehaviour
 {
+    [Tooltip("임시. Cook animation 실행 시간 (초)")]
+    public float Duration;
+    private float mCurrentDuration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mCurrentDuration = Duration;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        mCurrentDuration -= Time.fixedDeltaTime;
+        if (mCurrentDuration < 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     private void OnEnable()
