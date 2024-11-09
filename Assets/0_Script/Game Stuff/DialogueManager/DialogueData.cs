48d80156bccfbc554408228f8f4554984d9a2282
Alegruz <alegruz@khu.ac.kr>
2024-11-09 21:47:18 +0900

    - 대사 시스템 개선

---
+++ Assets/0_Script/Game Stuff/DialogueManager/DialogueData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueData : MonoBehaviour
+{
+    [System.Serializable]
+    public class Dialogue
+    {
+        [Tooltip("노출시킬 초상화")]
+        public Sprite sprite;
+        [Tooltip("대사")]
+        public string dialogue;
+    };
+
+    [Tooltip("대사 ID")]
+    public int Id;
+    [Tooltip("단순 인터랙션 시 대사 목록")]
+    public List<Dialogue> DialogueOnInteraction;
+
+    [Tooltip("대사 종료 시 이벤트")]
+    public UnityEvent OnDialogueEndEvent;
+
+    public void OnDialogueEnd() 
+    {
+        OnDialogueEndEvent.Invoke();
+    }
+}
