3b53dfcaca3b83febea8a09cecb4470acc439276
Song Min Gyeom <123460277+odd-mune@users.noreply.github.com>
2024-11-09 20:11:37 +0900

    - Camera Movement min,max position 전체 맵 적용 (hallway 오류)

--- Assets/0_Script/Game Stuff/CameraMovement.cs
+++ Assets/0_Script/Game Stuff/CameraMovement.cs
@@ -12,7 +12,7 @@ public class CameraMovement : MonoBehaviour
 
     void Start()
     {   
-        transform.position = new Vector3(target.position.x, target.position.y, target.position.z);
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
     }
 
     void LateUpdate()
@@ -20,7 +20,7 @@ public class CameraMovement : MonoBehaviour
         if(transform.position != target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x); 
             targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
