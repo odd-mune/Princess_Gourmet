3f76445c41a1bfb44197fda16684af70cdd097af
Alegruz <alegruz@khu.ac.kr>
2024-11-10 22:19:34 +0900

    - 게임 세이브 데이터 추가

---
+++ Assets/0_Script/Game Stuff/GameSaveData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New GameSaveData", menuName = "GameSaveData")]
+public class GameSaveData : ScriptableObject
+{
+    [Tooltip("플레이어 인벤토리")]
+    public PlayerInventory PlayerInventory;
+
+    public void Initialize()
+    {
+        PlayerInventory.Clear();
+    }
+}
