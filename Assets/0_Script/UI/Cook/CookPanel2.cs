fd7c3819a4c9cd1281ed4741b003ad93211fe760
Alegruz <alegruz@khu.ac.kr>
2024-11-12 19:58:16 +0900

    - build error

--- Assets/0_Script/UI/Cook/CookPanel2.cs
+++ Assets/0_Script/UI/Cook/CookPanel2.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.UI;
 
